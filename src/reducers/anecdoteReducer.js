9d04acb8da4bf599164a4845ed099cbcebe2b323
Sam Brodersen <sam.talebi84@gmail.com>
2020-04-24 18:13:32 +0200

    get anecdotes from backend

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -1,25 +1,4 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-];
-
-const getId = () => (100000 * Math.random()).toFixed(0);
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0,
-  };
-};
-
-const initialState = anecdotesAtStart.map(asObject);
-
-const reducer = (state = initialState, action) => {
+const reducer = (state = [], action) => {
   console.log('action', action);
   switch (action.type) {
     case 'INCREMENT':
@@ -35,6 +14,8 @@ const reducer = (state = initialState, action) => {
     case 'CREATE':
       const newAnecdote = action.data;
       return state.concat(newAnecdote);
+    case 'INIT':
+      return action.data;
     default:
       return state;
   }
@@ -43,7 +24,7 @@ const reducer = (state = initialState, action) => {
 export const createAnecdote = (content) => {
   return {
     type: 'CREATE',
-    data: { content, id: getId(), votes: 0 },
+    data: { content, votes: 0 },
   };
 };
 export const Voteincrement = (id) => {
@@ -51,6 +32,13 @@ export const Voteincrement = (id) => {
   return {
     type: 'INCREMENT',
     data: { id },
+  };
+};
+
+export const initialAnecdotes = (anecdotes) => {
+  return {
+    type: 'INIT',
+    data: anecdotes,
   };
 };
 
